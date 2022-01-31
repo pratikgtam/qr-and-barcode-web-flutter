6bd1d1ab057dd4051ca2573b252543f3c4f0556d
Pratik <hello.pratik@outlook.com>
2022-01-31 17:49:26 +0545

    first commit

---
+++ lib/home_screen.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:qr_web_flutter/qr_scan.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Scan QR code'),
+      ),
+      body: MyQRScanView(),
+    );
+  }
+}
